d205f6e6dbdd1975f34ed21efb161e5103ff4541
Richard Parsons <parsons.rk@gmail.com>
2019-09-19 12:28:29 +0200

    refactor control surface using prototype class

--- M4L/PushCtrl/Abstractions/controlSurface.js
+++ M4L/PushCtrl/Abstractions/controlSurface.js
@@ -1,36 +1,49 @@
-function ControlSurface(onOffControlName) {
-    this.isActive = false
-    this.onOffControl = null
-    this.controls = []
-    this.controlSurfaceApi = undefined
-    this.displayApi = []
-    this.sceneLaunchButtonsApi = undefined
-    this.controlNames = constants.pushControls
-    this.controlSurfaceApi = new LiveAPI('control_surfaces 0')
+var ControlSurface = (function() {
+    // Constructor
+    function ControlSurface(onOffControlName) {
+        this.isActive = false
+        this.onOffControl = null
+        this.controls = []
+        this.controlSurfaceApi = undefined
+        this.displayApi = []
+        this.sceneLaunchButtonsApi = undefined
+        this.controlNames = constants.pushControls
+        this.controlSurfaceApi = new LiveAPI('control_surfaces 0')
+        this.displayApi[0] = new LiveAPI(function() {}, getControlApi.call(this, 'Display_Line_0'))
+        this.displayApi[1] = new LiveAPI(function() {}, getControlApi.call(this, 'Display_Line_1'))
+        this.displayApi[2] = new LiveAPI(function() {}, getControlApi.call(this, 'Display_Line_2'))
+        this.displayApi[3] = new LiveAPI(function() {}, getControlApi.call(this, 'Display_Line_3'))
 
-    this.display = function(lineIndex, values) {
+        getControls.call(this, onOffControlName)
+        grabOnOffControl.call(this)
+        releaseControls.call(this)
+    }
+
+    // Public
+    ControlSurface.prototype.display = function(lineIndex, values) {
         if (this.isActive) {
-            this.displayApi[lineIndex].call('display_message', this._createDisplayMessage(values))
+            this.displayApi[lineIndex].call('display_message', createDisplayMessage.call(this, values))
         }
     }
 
-    this.toggleActive = function() {
-        this.isActive ? this._releaseControls() : this._grabControls()
+    ControlSurface.prototype.toggleActive = function() {
+        this.isActive ? releaseControls.call(this) : grabControls.call(this)
     }
 
-    this.onEncoderTurned = function(callback) {
-        this._observeControl('Track_Controls', callback)
+    ControlSurface.prototype.onEncoderTurned = function(callback) {
+        observeControl.call(this, 'Track_Controls', callback)
     }
 
-    this.onTapTempoButtonPressed = function(callback) {
-        this._observeControl('Tap_Tempo_Button', callback)
+    ControlSurface.prototype.onTapTempoButtonPressed = function(callback) {
+        observeControl.call(this, 'Tap_Tempo_Button', callback)
     }
 
-    this.onSceneLaunchButtonPressed = function(callback) {
-        this.sceneLaunchButtonsApi = this._observeControl('Scene_Launch_Buttons', callback)
+    ControlSurface.prototype.onSceneLaunchButtonPressed = function(callback) {
+        this.sceneLaunchButtonsApi = observeControl.call(this, 'Scene_Launch_Buttons', callback)
     }
 
-    this._observeControl = function(controlName, callback) {
+    // Private
+    function observeControl(controlName, callback) {
         const control = this.controlSurfaceApi.call('get_control_by_name', controlName)
         const controlApi = new LiveAPI(callback, control)
         controlApi.property = 'value'
@@ -38,11 +51,11 @@ function ControlSurface(onOffControlName) {
         return controlApi
     }
 
-    this._getControlApi = function(controlName) {
+    function getControlApi(controlName) {
         return this.controlSurfaceApi.call('get_control_by_name', [controlName])
     }
 
-    this._getControls = function(onOffControlName) {
+    function getControls(onOffControlName) {
         this.onOffControl = this.controlSurfaceApi.call('get_control_by_name', onOffControlName)
 
         for (var i in this.controlNames) {
@@ -52,43 +65,43 @@ function ControlSurface(onOffControlName) {
         }
     }
 
-    this._grabOnOffControl = function() {
+    function grabOnOffControl() {
         this.controlSurfaceApi.call('grab_control', this.onOffControl)
     }
 
-    this._grabControls = function() {
+    function grabControls() {
         this.isActive = true
 
-        this._enableObservers()
+        enableObservers.call(this)
 
         for (var i in this.controls) {
             this.controlSurfaceApi.call('grab_control', this.controls[i])
         }
     }
 
-    this._releaseControls = function() {
+    function releaseControls() {
         this.isActive = false
 
-        this._disableObservers()
+        disableObservers.call(this)
 
         for (var i in this.controls) {
             this.controlSurfaceApi.call('release_control', this.controls[i])
         }
     }
 
-    this._enableObservers = function() {
+    function enableObservers() {
         if (this.sceneLaunchButtonsApi) {
             this.sceneLaunchButtonsApi.property = 'value'
         }
     }
 
-    this._disableObservers = function() {
+    function disableObservers() {
         if (this.sceneLaunchButtonsApi) {
             this.sceneLaunchButtonsApi.property = ''
         }
     }
 
-    this._createDisplayMessage = function(messageItems) {
+    function createDisplayMessage(messageItems) {
         const paddingEnd = '        '
         var itemsPadded = []
 
@@ -101,12 +114,5 @@ function ControlSurface(onOffControlName) {
         return itemsPadded
     }
 
-    this.displayApi[0] = new LiveAPI(function() {}, this._getControlApi('Display_Line_0'))
-    this.displayApi[1] = new LiveAPI(function() {}, this._getControlApi('Display_Line_1'))
-    this.displayApi[2] = new LiveAPI(function() {}, this._getControlApi('Display_Line_2'))
-    this.displayApi[3] = new LiveAPI(function() {}, this._getControlApi('Display_Line_3'))
-
-    this._getControls(onOffControlName)
-    this._grabOnOffControl()
-    this._releaseControls()
-}
+    return ControlSurface
+})()
