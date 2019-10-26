9aca78b7ccfed8856009c7e10c1a48aecaab8ca2
Richard Parsons <parsons.rk@gmail.com>
2019-10-26 14:59:17 +0200

    refactored drumTrack as es6 class

--- M4L/PushCtrl/src/drum/drumTrack.js
+++ M4L/PushCtrl/src/drum/drumTrack.js
@@ -1,32 +1,34 @@
 import { mode } from '../constants'
 import { log } from '../util'
 
-export function DrumTrack(modes) {
-    this.modes = modes
-    this.activeMode = modes[0]
-    this.liveSetViewApi = new LiveAPI(null, 'live_set view')
-    this.trackId = parseInt(new LiveAPI(null, 'this_device canonical_parent').id)
-    this.liveSetTempoApi = new LiveAPI(this.handleTempoChange, 'live_set master_track mixer_device song_tempo')
-    this.liveSetTempoApi.property = 'value'
+export class DrumTrack {
+    constructor(modes) {
+        this.modes = modes
+        this.activeMode = modes[0]
+        this.liveSetViewApi = new LiveAPI(null, 'live_set view')
+        this.trackId = parseInt(new LiveAPI(null, 'this_device canonical_parent').id)
+        this.liveSetTempoApi = new LiveAPI(this.handleTempoChange, 'live_set master_track mixer_device song_tempo')
+        this.liveSetTempoApi.property = 'value'
+    }
 
-    this.getMode = function() {
+    getMode() {
         return this.activeMode
     }
 
-    this.setMode = function(modeType) {
+    setMode(modeType) {
         this.activeMode = this.modes.find(mode => mode.canHandle(modeType))
         this.activeMode.updateDisplay()
     }
 
-    this.handleTempoChange = function(args) {
+    handleTempoChange(args) {
         log('handleTempoChange', args)
-        // if (property === 'value') {
+        // if (property === 'value') {f
         //     this.activeMode.warpToProjectBpm(Number(bpm))
         //     this.activeMode.updateDisplay()
         // }
     }
 
-    this.toggleActive = function() {
+    toggleActive() {
         this.liveSetViewApi.set('selected_track', 'id', this.trackId)
 
         if (this.activeMode.canHandle(mode.INACTIVE)) {
