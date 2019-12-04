6b59c108068efaa91d7ee60ba4a6ceea3058e69c
Richard Parsons <parsons.rk@gmail.com>
2019-12-04 11:24:33 +0100

    renaming layer factory as chain

---
+++ src/factories/instrumentChainFactory.js
@@ -0,0 +1,20 @@
+import { InstrumentChain } from '../models/instrumentChain'
+import { createParameter } from './parameterFactory'
+import { createParameterPages } from './parameterPageFactory'
+
+export function createInstrumentChains(samplesFolder, instrumentRackName, pathToInstrumentRack, chainCount) {
+    var chains = []
+
+    for (var i = 0; i < chainCount; i++) {
+        const pathToChain = pathToInstrumentRack + ' chains ' + i
+        const chainApi = new LiveAPI(null, pathToChain)
+        const chainName = chainApi.get('name').toString()
+        const devicesCount = chainApi.get('devices').length / 2
+        const parameterPages = createParameterPages(samplesFolder, instrumentRackName, chainName, pathToChain, devicesCount)
+        const muteParameter = createParameter('Layer', 'Mute', pathToChain)
+
+        chains[i] = new InstrumentChain(i, chainName, parameterPages, muteParameter)
+    }
+
+    return chains
+}
--- src/factories/instrumentRackFactory.js
+++ src/factories/instrumentRackFactory.js
@@ -1,12 +1,12 @@
 import { InstrumentRack } from '../models/instrumentRack'
-import { createDrumLayers } from './drumLayerFactory'
+import { createInstrumentChains } from './instrumentChainFactory'
 import { createMixerPages } from './parameterPageFactory'
 
 export default function(samplesFolder, pathToInstrumentRack) {
     const instrumentRackApi = new LiveAPI(null, pathToInstrumentRack)
     const name = instrumentRackApi.get('name').toString()
     const chainCount = instrumentRackApi.get('chains').length / 2
-    const chains = createDrumLayers(samplesFolder, name, pathToInstrumentRack, chainCount)
+    const chains = createInstrumentChains(samplesFolder, name, pathToInstrumentRack, chainCount)
     const mixerPages = createMixerPages(pathToInstrumentRack, chainCount)
 
     return new InstrumentRack(name, chains, mixerPages)
