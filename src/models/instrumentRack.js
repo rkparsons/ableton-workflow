0e1928e99852c6249cb757c08bdc2ee03e67170d
Richard Parsons <parsons.rk@gmail.com>
2019-12-04 14:07:55 +0100

    extend rack from instrument rack

--- src/models/instrumentRack.js
+++ src/models/instrumentRack.js
@@ -1,10 +1,11 @@
-export class InstrumentRack {
+import { Rack } from './rack'
+
+export class InstrumentRack extends Rack {
     constructor(name, chains, mixerPages) {
+        super(mixerPages)
         this.name = name
         this.chains = chains
-        this.mixerPages = mixerPages
         this.activeChainIndex = 0
-        this.activeMixerPageIndex = 0
     }
 
     getName() {
@@ -12,8 +13,8 @@ export class InstrumentRack {
     }
 
     onValueChanged(callback) {
+        super.onValueChanged(callback)
         this.chains.forEach(chain => chain.onValueChanged(callback))
-        this.mixerPages.forEach(page => page.onValueChanged(callback))
     }
 
     getChains() {
@@ -34,17 +35,5 @@ export class InstrumentRack {
         if (this.activeChainIndex > 0) {
             this.activeChainIndex--
         }
-    }
-
-    getMixerPages() {
-        return this.mixerPages
-    }
-
-    getActiveMixerPage() {
-        return this.mixerPages[this.activeMixerPageIndex]
-    }
-
-    setActiveMixerPage(index) {
-        this.activeMixerPageIndex = index
     }
 }
