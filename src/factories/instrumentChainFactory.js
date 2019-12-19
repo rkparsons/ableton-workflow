b12de4a4f2acfba53b18882030cee925d1c990dc
Richard Parsons <parsons.rk@gmail.com>
2019-12-19 11:33:30 +0100

    moved devices out of parameter pages

--- src/factories/instrumentChainFactory.js
+++ src/factories/instrumentChainFactory.js
@@ -3,18 +3,38 @@ import { InstrumentChain } from '../models/instrumentChain'
 import { createParameterPages } from './parameterPageFactory'
 
 export function createInstrumentChains(samplesFolder, instrumentRackName, pathToInstrumentRack, chainCount) {
-    var chains = []
+    let chains = []
 
-    for (var i = 0; i < chainCount; i++) {
-        const pathToChain = pathToInstrumentRack + ' chains ' + i
-        const chainApi = new LiveAPI(null, pathToChain)
-        const chainName = chainApi.get('name').toString()
-        const devicesCount = chainApi.get('devices').length / 2
-        const parameterPages = createParameterPages(samplesFolder, instrumentRackName, chainName, pathToChain, devicesCount)
-        const muteParameter = new ChainMute({ pathToChain })
-
-        chains[i] = new InstrumentChain(i, chainName, parameterPages, muteParameter)
+    for (let chainIndex = 0; chainIndex < chainCount; chainIndex++) {
+        chains[chainIndex] = createInstrumentChain(samplesFolder, instrumentRackName, pathToInstrumentRack, chainIndex)
     }
 
     return chains
+}
+
+function createInstrumentChain(samplesFolder, instrumentRackName, pathToInstrumentRack, chainIndex) {
+    const pathToChain = pathToInstrumentRack + ' chains ' + chainIndex
+    const chainApi = new LiveAPI(null, pathToChain)
+    const chainName = chainApi.get('name').toString()
+    const devicesCount = chainApi.get('devices').length / 2
+    let deviceTypeToIndex = {}
+    let instrumentType = null
+
+    // todo: refactor device iteration into separate class
+    for (let deviceIndex = 0; deviceIndex < devicesCount; deviceIndex++) {
+        const deviceApi = new LiveAPI(null, pathToChain + ' devices ' + deviceIndex)
+        const deviceType = parseInt(deviceApi.get('type'))
+        const deviceName = deviceApi.get('name').toString()
+
+        deviceTypeToIndex[deviceName] = deviceIndex
+
+        if (deviceType === 1) {
+            instrumentType = instrumentRackName === 'Break' ? 'BreakSampler' : deviceName
+        }
+    }
+
+    const parameterPages = createParameterPages(samplesFolder, instrumentRackName, chainName, pathToChain, deviceTypeToIndex, instrumentType)
+    const muteParameter = new ChainMute({ pathToChain })
+
+    return new InstrumentChain(chainIndex, chainName, parameterPages, muteParameter)
 }
--- src/factories/parameterPageFactory.js
+++ src/factories/parameterPageFactory.js
@@ -3,23 +3,9 @@ import { getCategories, getSampleGroups } from '../util/fileSystem'
 import { ParameterPage } from '../models/parameterPage'
 import { parameterPageConfig } from '../config/parameterPageConfig'
 
-export function createParameterPages(samplesFolder, instrumentRackName, chainName, pathToChain, devicesCount) {
+export function createParameterPages(samplesFolder, instrumentRackName, chainName, pathToChain, deviceTypeToIndex, instrumentType) {
     //todo: replace vars with let
     var parameterPages = []
-    var deviceTypeToIndex = {}
-    var instrumentType = null
-
-    for (var deviceIndex = 0; deviceIndex < devicesCount; deviceIndex++) {
-        const deviceApi = new LiveAPI(null, pathToChain + ' devices ' + deviceIndex)
-        const deviceType = parseInt(deviceApi.get('type'))
-        const deviceName = deviceApi.get('name').toString()
-
-        deviceTypeToIndex[deviceName] = deviceIndex
-
-        if (deviceType === 1) {
-            instrumentType = instrumentRackName === 'Break' ? 'BreakSampler' : deviceName
-        }
-    }
 
     parameterPageConfig[instrumentType].forEach((page, index) => {
         const categories = getCategories(samplesFolder, instrumentRackName, chainName)
