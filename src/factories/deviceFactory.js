25213098196c860be31d3e648f691fcd05b754da
Richard Parsons <parsons.rk@gmail.com>
2019-12-19 16:06:25 +0100

    refactor device factory

--- src/factories/deviceFactory.js
+++ src/factories/deviceFactory.js
@@ -1,14 +1,24 @@
-import { createParameterPages } from './parameterPageFactory'
+import { getCategories, getSampleGroups } from '../util/fileSystem'
+
+import { ParameterPage } from '../models/parameterPage'
+import { parameterPageConfig } from '../config/parameterPageConfig'
 
 export const createDevice = {
     Instrument: {
         Sampler: (samplesFolder, instrumentRackName, chainName, pathToChain, deviceIndex) => {
-            return createParameterPages(samplesFolder, instrumentRackName, chainName, pathToChain, deviceIndex, 'Sampler')
+            const categories = getCategories(samplesFolder, instrumentRackName, chainName)
+            const samples = getSampleGroups(samplesFolder, instrumentRackName, chainName, categories)
+
+            return parameterPageConfig['Sampler'].map((page, index) => {
+                const parameters = page.parameters.map(ParameterClass => new ParameterClass({ pathToChain, deviceIndex, options: categories, optionGroups: samples }))
+
+                return new ParameterPage(index, page.name, parameters)
+            })
         },
 
-        BreakSampler: (samplesFolder, instrumentRackName, chainName, pathToChain, deviceIndex) => {
-            return createParameterPages(samplesFolder, instrumentRackName, chainName, pathToChain, deviceIndex, 'BreakSampler')
-        },
+        // BreakSampler: (samplesFolder, instrumentRackName, chainName, pathToChain, deviceIndex) => {
+        //     return createParameterPages(samplesFolder, instrumentRackName, chainName, pathToChain, deviceIndex, 'BreakSampler')
+        // },
     },
 
     MidiEffect: {},
--- src/factories/parameterPageFactory.js
+++ src/factories/parameterPageFactory.js
@@ -1,22 +1,5 @@
-import { getCategories, getSampleGroups } from '../util/fileSystem'
-
 import { ParameterPage } from '../models/parameterPage'
 import { parameterPageConfig } from '../config/parameterPageConfig'
-
-export function createParameterPages(samplesFolder, instrumentRackName, chainName, pathToChain, deviceIndex, instrumentType) {
-    //todo: replace vars with let
-    var parameterPages = []
-
-    parameterPageConfig[instrumentType].forEach((page, index) => {
-        const categories = getCategories(samplesFolder, instrumentRackName, chainName)
-        const samples = getSampleGroups(samplesFolder, instrumentRackName, chainName, categories)
-        const parameters = page.parameters.map(ParameterClass => new ParameterClass({ pathToChain, deviceIndex, options: categories, optionGroups: samples }))
-
-        parameterPages.push(new ParameterPage(index, page.name, parameters))
-    })
-
-    return parameterPages
-}
 
 export function createMixerPages(pathToRack, chainCount) {
     const mixerPages = []
