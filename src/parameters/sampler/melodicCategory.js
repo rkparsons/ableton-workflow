cb51568c04a90c7edc391f4f545408a719899b6b
Richard Parsons <parsons.rk@gmail.com>
2019-12-22 10:44:37 +0100

    track group from category

--- src/parameters/sampler/melodicCategory.js
+++ src/parameters/sampler/melodicCategory.js
@@ -1,7 +1,39 @@
 import { EnumParameter } from '../../models/enumParameter'
 
 export class SamplerMelodicCategory extends EnumParameter {
-    constructor({ pathToChain, deviceIndex, options }) {
+    constructor({ pathToChain, deviceIndex, options, optionGroups }) {
         super({ name: 'Category', basePath: `${pathToChain} devices ${deviceIndex}`, path: 'parameters 3', options, isCategory: true })
+        this.categorySizes = this.getCategorySizes(optionGroups)
+    }
+
+    observeValue([property, chainSelector]) {
+        if (property === this.property) {
+            this.value = this.getCategoryIndex(chainSelector)
+            this.callback()
+        }
+    }
+
+    getCategorySizes(optionGroups) {
+        let sizes = []
+
+        for (key in optionGroups) {
+            sizes.push(optionGroups[key].length)
+        }
+
+        return sizes
+    }
+
+    getCategoryIndex(chainSelector) {
+        let sum = 0
+
+        for (let categoryIndex = 0; categoryIndex < this.categorySizes.length; categoryIndex++) {
+            const nextCategorySize = this.categorySizes[categoryIndex]
+
+            if (chainSelector < sum + nextCategorySize) {
+                return categoryIndex
+            }
+
+            sum += nextCategorySize
+        }
     }
 }
