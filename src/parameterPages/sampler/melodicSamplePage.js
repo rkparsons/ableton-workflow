df92fe944199029373437e0db4873c1902e122d0
Richard Parsons <parsons.rk@gmail.com>
2020-03-08 11:43:25 +0100

    Remove repitch from melodic sample page

--- src/parameterPages/sampler/melodicSamplePage.js
+++ src/parameterPages/sampler/melodicSamplePage.js
@@ -3,7 +3,6 @@ import { ChainSolo } from '../../parameters/chain/solo'
 import { SamplePage } from './samplePage'
 import { SamplerMelodicCategory } from '../../parameters/sampler/melodicCategory'
 import { SamplerMelodicSelect } from '../../parameters/sampler/melodicSelect'
-import { SamplerRepitch } from '../../parameters/sampler/repitch'
 import { SamplerReverse } from '../../parameters/sampler/reverse'
 
 export class MelodicSamplePage extends SamplePage {
@@ -15,7 +14,6 @@ export class MelodicSamplePage extends SamplePage {
             new CCStart({ pathToChain, deviceIndex }),
             new SamplerReverse({ pathToChain, deviceIndex }),
             new ChainSolo({ pathToChain, deviceIndex }),
-            new SamplerRepitch({ pathToChain, deviceIndex }),
         ]
 
         super(pageIndex, parameters)
