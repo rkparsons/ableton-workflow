ef4166e4ce4211ef3e719b91ea1f232e12bc472b
Richard Parsons <parsons.rk@gmail.com>
2020-03-08 12:04:10 +0100

    Add mallet velocity

--- src/parameterPages/collision/malletPage.js
+++ src/parameterPages/collision/malletPage.js
@@ -1,9 +1,10 @@
 import { CollisionMalletVolume } from '../../parameters/collision/malletVolume'
+import { CollisionMalletVolumeVelocity } from '../../parameters/collision/malletVolumeVelocity'
 import { ParameterPage } from '../../models/parameterPage'
 
 export class MalletPage extends ParameterPage {
     constructor(pageIndex, pathToChain, deviceIndex) {
-        const parameters = [new CollisionMalletVolume({ pathToChain, deviceIndex })]
+        const parameters = [new CollisionMalletVolume({ pathToChain, deviceIndex }), new CollisionMalletVolumeVelocity({ pathToChain, deviceIndex })]
 
         super(pageIndex, 'Mallet', parameters)
     }
---
+++ src/parameters/collision/malletVolumeVelocity.js
@@ -0,0 +1,12 @@
+import { ValueParameter } from '../../models/valueParameter'
+
+export class CollisionMalletVolumeVelocity extends ValueParameter {
+    constructor({ pathToChain, deviceIndex }) {
+        super({
+            name: '< vel',
+            basePath: `${pathToChain} devices ${deviceIndex}`,
+            path: 'parameters 8',
+            inputRange: [-5, 5],
+        })
+    }
+}
