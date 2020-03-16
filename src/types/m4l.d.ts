9e67fc546d6fa0548d509857207aaa1839bfc123
Richard Parsons <parsons.rk@gmail.com>
2020-03-16 16:58:34 +0100

    Use module syntax for typings

--- src/types/m4l.d.ts
+++ src/types/m4l.d.ts
@@ -1,11 +1,11 @@
-export class LiveAPI {
+declare class LiveAPI {
     constructor(callback: () => void, path: string)
 }
 
-export class Folder {
+declare class Folder {
     constructor(path: string)
 
     typelist: string[]
 }
 
-export function post(input: string): void
+declare function post(input: string): void
