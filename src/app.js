e404eb44b124f04f9868d04838fdd9431d9127ee
Richard Parsons <parsons.rk@gmail.com>
2020-03-17 13:00:27 +0100

    Auto reload patch

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
+/* eslint-disable */
+
 import { createLiveset } from '~/factories/livesetFactory'
 import log from '~/util/log'
+
+log('Compiling...')
+log('Checking for LiveAPI...')
+outlet(0, 'bang')
 
 export default function init() {
     log('init')
