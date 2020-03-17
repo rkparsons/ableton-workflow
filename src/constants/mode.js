5da06adcd21c70edb030af3bfe0e4c20e47ee2b2
Richard Parsons <parsons.rk@gmail.com>
2020-03-17 12:10:56 +0100

    Convert mode to ts

--- src/constants/mode.js
+++
@@ -1,9 +0,0 @@
-export default {
-    INACTIVE: 0,
-    CHAIN_PARAMS: 1,
-    CHAIN_FX: 2,
-    INSTRUMENT_RACK_MIXER: 3,
-    INSTRUMENT_RACK_FX: 4,
-    DRUM_RACK_MIXER: 5,
-    DRUM_RACK_FX: 6,
-}
---
+++ src/constants/mode.ts
@@ -0,0 +1,11 @@
+enum Mode {
+    INACTIVE = 0,
+    CHAIN_PARAMS = 1,
+    CHAIN_FX = 2,
+    INSTRUMENT_RACK_MIXER = 3,
+    INSTRUMENT_RACK_FX = 4,
+    DRUM_RACK_MIXER = 5,
+    DRUM_RACK_FX = 6,
+}
+
+export default Mode
