7dcebda97a88052c52a66c33567bb8a507693258
Richard Parsons <parsons.rk@gmail.com>
2020-03-18 16:04:34 +0100

    Refactor value parameter as ts

--- src/models/valueParameter.ts
+++ src/models/valueParameter.ts
@@ -1,8 +1,27 @@
+import ASCII from '../constants/ascii'
 import { Parameter } from './parameter'
-import ascii from '../constants/ascii.ts'
+import UnitType from '../constants/unitType'
+
+type Props = {
+    name: string
+    basePath: string
+    path: string
+    property: string
+    defaultValue: number
+    unitType: UnitType
+    inputRange: number[]
+    randomRange?: number[]
+    showValue: boolean
+    speed: number
+    isBpm: boolean
+}
 
 export class ValueParameter extends Parameter {
-    constructor({ name, basePath, path, property, defaultValue, unitType, inputRange, randomRange, showValue, speed, isBpm }) {
+    inputRange: number[]
+    isBipolar: boolean
+    isBpm: boolean
+
+    constructor({ name, basePath, path, property, defaultValue, unitType, inputRange, randomRange, showValue, speed, isBpm }: Props) {
         super({ name, basePath, path, property, defaultValue, unitType, randomRange, speed })
         this.inputRange = inputRange || [0, 1]
         this.min = this.inputRange[0]
@@ -17,12 +36,12 @@ export class ValueParameter extends Parameter {
             return ' '
         }
 
-        return this.showValue ? super.getOutputValue() : this.getDisplayValueGraphic()
+        return this.showValue ? super.getOutputValue().toString() : this.getDisplayValueGraphic()
     }
 
     getDisplayValueGraphic() {
         const barCount = this.isBipolar ? 4 : 8
-        const padding = String.fromCharCode(ascii.EMPTY_BARS, ascii.EMPTY_BARS, ascii.EMPTY_BARS, ascii.EMPTY_BARS, ascii.EMPTY_BARS, ascii.EMPTY_BARS, ascii.EMPTY_BARS, ascii.EMPTY_BARS)
+        const padding = String.fromCharCode(...new Array<number>(8).fill(ASCII.EMPTY_BARS))
         const outputPositive = this.isBipolar && this.value < 0 ? '' : this.getMeterOutput(true, barCount, this.value / this.max)
         const outputNegative = this.isBipolar && this.value > 0 ? '' : this.getMeterOutput(false, barCount, this.min === 0 ? 0 : this.value / this.min)
 
@@ -33,25 +52,25 @@ export class ValueParameter extends Parameter {
         }
     }
 
-    getIncrement(delta) {
+    getIncrement(delta: number) {
         return (this.speed * ((this.max - this.min) * (delta < 50 ? delta : delta - 128))) / 100
     }
 
-    getMeterOutput(isPositive, barCount, fraction) {
+    getMeterOutput(isPositive: boolean, barCount: number, fraction: number) {
         const fullBars = Math.floor(barCount * fraction)
         const isHalfBar = Math.abs(barCount * fraction) % 1 >= 0.5
         var output = ''
 
         if (!isPositive && isHalfBar) {
-            output += String.fromCharCode(ascii.MINUS_BAR)
+            output += String.fromCharCode(ASCII.MINUS_BAR)
         }
 
         for (var i = 0; i < fullBars; i++) {
-            output += String.fromCharCode(ascii.TWO_BARS)
+            output += String.fromCharCode(ASCII.TWO_BARS)
         }
 
         if (isPositive && isHalfBar) {
-            output += String.fromCharCode(ascii.PLUS_BAR)
+            output += String.fromCharCode(ASCII.PLUS_BAR)
         }
 
         return output
