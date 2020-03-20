cd37c55377063ad02297f27c91311f99f8b92be6
Richard Parsons <parsons.rk@gmail.com>
2020-03-20 12:29:57 +0100

    Remove comments

--- src/app.js
+++ src/app.js
@@ -5,10 +5,7 @@ import log from '~/util/log'
 
 log('Compiling...')
 log('Checking for LiveAPI...')
-
-if (this) {
-    outlet(0, 'bang')
-}
+outlet(0, 'bang')
 
 export default function init() {
     log('Initialised!')
