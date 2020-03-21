174fabf7b78adfff7659811d04d9ece4891ccd96
Richard Parsons <parsons.rk@gmail.com>
2020-03-21 12:03:58 +0100

    Refactor filtered enum param

--- src/models/filteredEnumParameter.ts
+++ src/models/filteredEnumParameter.ts
@@ -1,18 +1,28 @@
 import { EnumParameter } from './enumParameter'
 
+type Props = {
+    name: string
+    basePath: string
+    path?: string
+    property: string
+    defaultValue?: number
+    optionGroups: Record<string, string[]>
+    isSample: boolean
+}
+
 export class FilteredEnumParameter extends EnumParameter {
-    constructor({ name, basePath, path, property, defaultValue, optionGroups = {}, isSample }) {
+    optionGroups: Record<string, string[]>
+    isSample: boolean
+    constructor({ name, basePath, path, property, defaultValue, optionGroups = {}, isSample }: Props) {
         super({ name, basePath, path, property, defaultValue, options: optionGroups[Object.keys(optionGroups)[0]] })
         this.optionGroups = optionGroups
         this.isSample = isSample
     }
 
-    filterOptions(optionGroupKey) {
+    filterOptions(optionGroupKey: string) {
         this.value = 0
         this.options = this.optionGroups[optionGroupKey]
-        this.optionKeys = Object.keys(this.options)
-        this.min = this.optionKeys[0]
-        this.max = this.optionKeys[this.optionKeys.length - 1]
+        this.max = this.options.length - 1
         this.randomRange = [this.min, this.max]
     }
 }
