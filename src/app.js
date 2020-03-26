613fc66836ecf4619ec2d75c53884cb1e1452280
Richard Parsons <parsons.rk@gmail.com>
2020-03-26 15:07:40 +0100

    Simplify init logic to avoid polling

--- src/app.js
+++ src/app.js
@@ -2,11 +2,6 @@
 
 import { createLiveset } from '~/factories/livesetFactory'
 
-let isInitialised = false
 export default function init() {
-    if (!isInitialised) {
-        createLiveset(this.patcher.filepath)
-        isInitialised = true
-        outlet(0, 'Ready!')
-    }
+    createLiveset(this.patcher.filepath)
 }
