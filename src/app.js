4b0edc826f3f8591b544754c9af5d5bdc812c695
Richard Parsons <parsons.rk@gmail.com>
2020-03-26 16:13:06 +0100

    Use messnamed for logging to console

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 /* eslint-disable */
 
 import { createLiveset } from '~/factories/livesetFactory'
-import log from '~/util/log'
 
 messnamed('init', 'bang')
 
 export default function init() {
     createLiveset(this.patcher.filepath)
-    log('Ready!')
+    messnamed('console', 'Ready!')
 }
