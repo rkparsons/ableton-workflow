95c404fc1e6d4f26332d2c453b367f134d470e41
Richard Parsons <parsons.rk@gmail.com>
2020-07-04 19:13:36 +0200

    Add fm freq

--- src/parameterPages/omnisphere/fmPage.ts
+++ src/parameterPages/omnisphere/fmPage.ts
@@ -1,10 +1,11 @@
+import { OmnisphereFMFreq } from '~/parameters/omnisphere/fmFreq'
 import { OmnisphereFMOn } from '~/parameters/omnisphere/fmOn'
 import { OmnisphereFMTracking } from '~/parameters/omnisphere/fmTracking'
 import { ParameterPage } from '~/models/parameterPage'
 
 export class FMPage extends ParameterPage {
     constructor(pageIndex: number, pathToChain: string, deviceIndex: number) {
-        const parameters = [new OmnisphereFMOn({ pathToChain, deviceIndex }), new OmnisphereFMTracking({ pathToChain, deviceIndex })]
+        const parameters = [new OmnisphereFMOn({ pathToChain, deviceIndex }), new OmnisphereFMTracking({ pathToChain, deviceIndex }), new OmnisphereFMFreq({ pathToChain, deviceIndex })]
 
         super(pageIndex, 'FM', parameters)
     }
